32b0319eb4095805fe91c04f97fcfa3d40cdedf5
Andi Angel <andi@bitweb.ee>
2021-01-11 12:11:40 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import background from './download.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="back-to-the-future">
+      <img src={background}></img>
+      <h1 className="the-message">
+Sorry! This page doesn't support Internet Explorer
+      </h1>
+      <h2>Step into the 21st century</h2>
+      <h3>Get <a href="https://www.google.com/chrome/">Chrome</a>!</h3>
+    </div>
+  );
+}
+
+export default App;
